72b46088a2073910bec6a388279bfd3f399aeb93
noufa <noufa@DESKTOP-KJL7CI4>
2022-08-26 14:06:30 +0530

    Add project files.

---
+++ FirstMobApp/FirstMobApp/App.xaml.cs
@@ -0,0 +1,33 @@
+﻿using FirstMobApp.Services;
+using FirstMobApp.Views;
+using System;
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+namespace FirstMobApp
+{
+    public partial class App : Application
+    {
+
+        public App()
+        {
+            InitializeComponent();
+
+            //DependencyService.Register<MockDataStore>();
+            //MainPage = new AppShell();
+            MainPage = new NavigationPage(new Login());
+        }
+
+        protected override void OnStart()
+        {
+        }
+
+        protected override void OnSleep()
+        {
+        }
+
+        protected override void OnResume()
+        {
+        }
+    }
+}
---
+++ FirstMobApp/FirstMobApp/Views/Login.xaml.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+namespace FirstMobApp.Views
+{
+    [XamlCompilation(XamlCompilationOptions.Compile)]
+    public partial class Login : ContentPage
+    {
+        public Login()
+        {
+            
+            InitializeComponent();
+            this.EntryErrorMessage.IsVisible = false;
+        }
+        async void onButtonClicked(Object sender, EventArgs args)
+        {
+            this.EntryErrorMessage.IsVisible = false;
+            var userName = this.EntryUserName.Text;
+            var password = this.EntryPassword.Text;
+            if(String.IsNullOrEmpty(userName) || String.IsNullOrEmpty(password))
+            {
+                this.EntryErrorMessage.IsVisible = true;
+                this.EntryErrorMessage.Text = "Invalid username/password";               
+            }
+            else if(userName.ToLower()=="admin" && password == "password")
+            {
+               await Navigation.PushAsync(new HomePage());
+            }
+            else
+            {
+                await Navigation.PushAsync(new ErrorPage());
+            }          
+        }
+
+    }
+}
